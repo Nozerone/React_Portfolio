7b14c27c9e3584669529ab652b1fe099dc1585ec
Nozerone <134429390+Nozerone@users.noreply.github.com>
2023-11-22 00:37:54 -0800

    styling to footer element

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,10 +4,10 @@ import { GithubIcon, Instagram, Linkedin } from "lucide-react";
 const Footer = ({}) => {
  return (
    <>
-     <div>
-       <footer className="card text-bg-secondary mb-3 ">
-         <div className="card-body">
-           <div style={{ display: 'flex', justifyContent: 'center' }}>
+     <div >
+       <footer>
+           <div className="row">
+         <div className="col d-flex flex-row gap-3 justify-content-center mb-4 mt-4">
              <a href="https://github.com/Nozerone"><GithubIcon color="red" size={48} /></a>
              <a href="https://www.linkedin.com/in/jose-posadas-7b980a203/"><Linkedin color="blue" size={48} /></a>
              <a href="https://www.instagram.com/nozerone"><Instagram color="green" size={48} /></a>
